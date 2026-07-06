ae67de002675f7302bd9eb709e6601939f510f04
tanushka-devabattula <tanushka7525@gmail.com>
2026-07-06 11:26:16 +0530

    Update pdf_utils.py

--- pdf_utils.py
+++ pdf_utils.py
@@ -1,1 +1,15 @@
+from pypdf import PdfReader
 
+
+def extract_text_from_pdf(uploaded_file):
+    reader = PdfReader(uploaded_file)
+
+    text = ""
+
+    for page in reader.pages:
+        extracted = page.extract_text()
+
+        if extracted:
+            text += extracted + "\n"
+
+    return text
